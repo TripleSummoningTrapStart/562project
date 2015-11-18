6cd7141283fc27413178c21c24fe974b1617e640
23rcw5 <23rcw5@gmail.com>
2015-11-18 16:11:05 -0600

    added insert functions

--- TriggersAndSQL.sql
+++ TriggersAndSQL.sql
@@ -42,3 +42,45 @@ insert into solutions(Max_Percentage,JSonString,AssignmentId) values(percentage,
 end
 $$
 language'plpgsql'
+
+
+
+CREATE FUNCTION addGrade(Graded_Against_SolutionId int, SubmissionId int, Grade int, GradeComments varchar(Max)) returns void as $$
+begin
+insert into Graded values (Graded_Against_SolutionId, SubmissionId , Grade, GradeComments  )
+
+end
+$$
+language'plpgsql'
+
+
+CREATE FUNCTION addAssignment( InstructorId int, DueDate DateTime) returns void as $$
+begin
+insert into Assignments values (InstructorId, DueDate)
+end
+$$
+language'plpgsql'
+
+
+CREATE FUNCTION addSubmissions( AssignmentId int, StudentId int, JSonString varchar(max), SubmissionDate DATETIME) returns void as $$
+begin
+insert into Submissions values (AssignmentId , StudentId,  JSonString,  SubmissionDate )
+end
+$$
+language'plpgsql'
+
+
+CREATE FUNCTION addSolutions( Max_Percentage int, JSonString varchar, SubmissionDate DATETIME) returns void as $$
+begin
+insert into Solutions values (Max_Percentage  , JSonString ,  SubmissionDate )
+end
+$$
+language'plpgsql'
+
+CREATE FUNCTION addPeople( isIntructor bit, First_Name varchar(50), Last_Name varchar(50)) returns void as $$
+begin
+insert into People values (isIntructor   , First_Name ,  Last_Name )
+end
+$$
+language'plpgsql'
+
