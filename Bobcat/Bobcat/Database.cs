bbc9ecabc33afe80f31437885a922c8227cff158
TripleSummoningTrapStart <Fatalkirby@gmail.com>
2015-12-01 13:56:13 -0600

    added various files

---
+++ Bobcat/Bobcat/Database.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bobcat
+{
+    public class Columns
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public bool? nn { get; set; }
+    }
+
+    public class Database
+    {
+        public string name { get; set; }
+        public List<int> pos { get; set; }
+        public List<List<string>> uq { get; set; }
+        public List<Columns> cols { get; set; }
+
+        public void addSubmission(string json)
+        {
+
+        }
+        public void Grade(string json, int assignmentID)
+        {
+            List<Columns> solutionsColumns = getSolutions(assignmentID);
+            List<Columns> submissionColumns = getSubmissionColumns(json);
+            List<Tuple<int, int>> differences = new List<Tuple<int, int>>();
+
+            int match;
+            int bestMatchIndex = -1;
+
+            for(int i = 0; i < solutionsColumns.Count; i++)
+            {
+                match = 0;
+                for(int k = 0; k < submissionColumns.Count; k++)
+                {
+                    if (solutionsColumns[i].id == submissionColumns[k].id)
+                    {
+                        ++match;
+                    }
+                    
+                }
+                if(match == submissionColumns.Count)
+                {
+                    bestMatchIndex = i;
+                }
+                else
+                {
+                    differences.Add(new Tuple<int, int>(i, (Math.Abs(solutionsColumns.Count - match)+Math.Abs(submissionColumns.Count - solutionsColumns.Count))));
+                }
+                
+            }
+            if(bestMatchIndex == -1)
+            {
+                bestMatchIndex = findClosestMatch(differences);
+            }
+        }
+        public List<Columns> getSolutions(int assignmentID)
+        {
+            return null;
+        }
+        public List<Columns> getSubmissionColumns(string json)
+        {
+            return null;
+        }
+        public int findClosestMatch(List<Tuple<int, int>> differences)
+        {
+            int closestDifference = Int32.MaxValue;
+            int closestIndex = -1;
+            foreach(Tuple<int, int> t in differences)
+            {
+                if(t.Item2 < closestDifference)
+                {
+                    closestDifference = t.Item2;
+                    closestIndex = t.Item1;
+                }
+            }
+            return closestIndex;
+        }
+    }
+
+
+}
---
+++ ReadInSoutions/ReadInSoutions/Database.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.IO;
+using System.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
+namespace ReadInSoutions
+{
+    public class Columns
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public bool? nn { get; set; }
+    }
+
+    public class Database
+    {
+        public string name { get; set; }
+        public List<int> pos { get; set; }
+        public List<List<string>> uq { get; set; }
+        public List<Columns> cols { get; set; }
+    }
+    public class SolutionDatabase
+    {
+        public Database database { get; set; }
+        public int grade { get; set; }
+        public int solId { get; set; }
+
+        public SolutionDatabase(Database d, int g, int s)
+        {
+            database = d;
+            grade = g;
+            solId = s;
+        }
+    }
+    public class functions
+    {
+        NpgsqlConnection conn;
+
+        public functions()
+        {
+            conn = new NpgsqlConnection("Host=127.0.0.1;Port=5432;Username=postgres;Password=hello;Database=562Project");
+            conn.Open();
+        }
+        public int addSubmission(string json)
+        {
+            NpgsqlCommand command = new NpgsqlCommand("addSubmisson", conn);
+            command.CommandType = CommandType.StoredProcedure;
+
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = "AssignmentId";
+            parameter.DbType = System.Data.DbType.Int32;
+            parameter.Value = 1;
+            command.Parameters.Add(parameter);
+
+            var parameter2 = command.CreateParameter();
+            parameter2.ParameterName = "StudentId";
+            parameter2.DbType = System.Data.DbType.Int32;
+            parameter2.Value = 1;
+            command.Parameters.Add(parameter2);
+
+            var parameter3 = command.CreateParameter();
+            parameter3.ParameterName = "JSonString";
+            parameter3.DbType = System.Data.DbType.AnsiString;
+            parameter3.Value = json;
+            command.Parameters.Add(parameter3);
+            command.ExecuteNonQuery();
+
+            return Grade(json, 1);
+        }
+        public int Grade(string json, int assignmentID)
+        {
+            List<SolutionDatabase> solutions = getSolutions(assignmentID);
+            List<Columns> submissionColumns = getSubmissionColumns(json);
+            List<Tuple<int, int>> differences = new List<Tuple<int, int>>();
+
+            int match;
+            int bestMatchIndex = -1;
+            for(int m = 0; m < solutions.Count; m++)
+            {
+                for (int i = 0; i < solutions[m].database.cols.Count; i++)
+                {
+                    match = 0;
+                    for (int k = 0; k < submissionColumns.Count; k++)
+                    {
+                        if (solutions[m].database.cols[i].id == submissionColumns[k].id)
+                        {
+                            match++;
+                        }
+
+                    }
+                    if (match == solutions[m].database.cols.Count)
+                    {
+                        bestMatchIndex = i;
+                    }
+                    else
+                    {
+                        differences.Add(new Tuple<int, int>(i, (Math.Abs(solutions[m].database.cols.Count - match) + Math.Abs(submissionColumns.Count - solutions[m].database.cols.Count))));
+                    }
+
+                }
+            }
+           
+            if (bestMatchIndex == -1)
+            {
+                bestMatchIndex = findClosestMatch(differences);
+            }
+            addGrade(bestMatchIndex, solutions[bestMatchIndex]);
+            return solutions[bestMatchIndex].grade;
+        }
+        public void addGrade(int index, SolutionDatabase s)
+        {
+            NpgsqlCommand command = new NpgsqlCommand("addGrade", conn);
+            command.CommandType = CommandType.StoredProcedure;
+
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = "Graded_Against_SolutionId";
+            parameter.DbType = System.Data.DbType.Int32;
+            parameter.Value = s.solId;
+            command.Parameters.Add(parameter);
+
+            var parameter2 = command.CreateParameter();
+            parameter2.ParameterName = "SubmissionId";
+            parameter2.DbType = System.Data.DbType.Int32;
+            parameter2.Value = 1;
+            command.Parameters.Add(parameter2);
+
+            var parameter3 = command.CreateParameter();
+            parameter3.ParameterName = "Grade";
+            parameter3.DbType = System.Data.DbType.Int32;
+            parameter3.Value = s.grade;
+            command.Parameters.Add(parameter3);
+            command.ExecuteNonQuery();
+
+            var parameter4 = command.CreateParameter();
+            parameter4.ParameterName = "GradeComments";
+            parameter4.DbType = System.Data.DbType.AnsiString;
+            parameter4.Value = string.Empty;
+            command.Parameters.Add(parameter4);
+            command.ExecuteNonQuery();
+        }
+        public List<SolutionDatabase> getSolutions(int assignmentID)
+        {
+            List<SolutionDatabase> solutions = new List<SolutionDatabase>();
+            NpgsqlCommand command = new NpgsqlCommand("getSolution", conn);
+            command.CommandType = CommandType.StoredProcedure;
+
+            var parameter = command.CreateParameter();
+            parameter.ParameterName = "AssnId";
+            parameter.DbType = System.Data.DbType.Int32;
+            parameter.Value = assignmentID;
+            command.Parameters.Add(parameter);
+
+            NpgsqlDataReader dr = command.ExecuteReader();
+
+
+            while (dr.Read())
+            {
+                Database d = JsonConvert.DeserializeObject<Database>(dr[3].ToString());
+                solutions.Add(new SolutionDatabase(d, (int)dr[2], (int)dr[0]));
+            }
+                
+                
+
+            return solutions;
+        }
+        public List<Columns> getSubmissionColumns(string json)
+        {
+            Database d = JsonConvert.DeserializeObject<Database>(json);
+            return d.cols;
+        }
+        public int findClosestMatch(List<Tuple<int, int>> differences)
+        {
+            int closestDifference = Int32.MaxValue;
+            int closestIndex = -1;
+            foreach (Tuple<int, int> t in differences)
+            {
+                if (t.Item2 < closestDifference)
+                {
+                    closestDifference = t.Item2;
+                    closestIndex = t.Item1;
+                }
+            }
+            return closestIndex;
+        }
+        public void addSolutions()
+        {
+            List<Solution> solutions = new List<Solution>();
+            string[] lines = File.ReadAllLines(@"E:\School\Senior Semester 1\CIS 562\562project\Solutions.txt");
+            int assignmentID = Convert.ToInt32(lines[0]);
+            int num = 1;
+            foreach (string s in lines)
+            {
+                if (num == 1)
+                {
+                    num++;
+                }
+                else
+                {
+                    string[] split = s.Split('|');
+                    int percentage = Convert.ToInt32(split[0]);
+                    string json = split[1];
+                    solutions.Add(new Solution(percentage, json, assignmentID));
+                }
+            }
+
+
+            foreach (Solution s in solutions)
+            {
+
+                NpgsqlCommand command = new NpgsqlCommand("addSolution", conn);
+                command.CommandType = CommandType.StoredProcedure;
+
+                var parameter = command.CreateParameter();
+                parameter.ParameterName = "percentage";
+                parameter.DbType = System.Data.DbType.Int32;
+                parameter.Value = s.percentage;
+                command.Parameters.Add(parameter);
+
+                var parameter2 = command.CreateParameter();
+                parameter2.ParameterName = "jsonString";
+                parameter2.DbType = System.Data.DbType.AnsiString;
+                parameter2.Value = s.json;
+                command.Parameters.Add(parameter2);
+
+                var parameter3 = command.CreateParameter();
+                parameter3.ParameterName = "AssnId";
+                parameter3.DbType = System.Data.DbType.Int32;
+                parameter3.Value = s.assignmentId;
+                command.Parameters.Add(parameter3);
+                command.ExecuteNonQuery();
+
+            }
+        }
+        public void CloseConnections()
+        {
+            conn.Close();
+        }
+    }
+
+
+}
